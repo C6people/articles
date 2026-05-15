874fe3f06be2e646ebbe8c72f59e207379920cc0
K-okamoto213 <kd1374075@st.kobedenshi.ac.jp>
2026-05-15 13:50:50 +0900

    マージ修正

--- app/src/views/UserProfileView.vue
+++ app/src/views/UserProfileView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import axios from 'axios';
 import { useRouter } from 'vue-router';
 import CommonHeader from '@/components/CommonHeader.vue';
